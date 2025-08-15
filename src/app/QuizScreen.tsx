6caf85720fc3301e879db9bdbe86de2cc01b005e
Edward <onirotto7585@pm.me>
2025-08-15 09:47:25 +0700

    UI-2

--- src/app/QuizScreen.tsx
+++ src/app/QuizScreen.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Pressable } from 'react-native';
 import QuestionCard from '../components/QuestionCard';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
 
 const QuizScreen = () => {
     return (
-        <View style={styles.container}>
-            <QuestionCard />
-        </View>
+        <SafeAreaView style={styles.page}>
+            <View style={styles.container}>
+                {/* Header */}
+                <View>
+                    <Text style={styles.title}>Question 1/5</Text>
+                </View>
+
+                {/* Body */}
+                <View>
+                    <QuestionCard />
+                    <Text style={styles.time}>20 seconds</Text>
+                </View>
+
+                {/* Footer */}
+                <Pressable style={styles.button} onPress={() => console.warn('pressed')}>
+                    <Text style={styles.buttonText}>Next</Text>
+                    <FontAwesome6
+                        name='arrow-right-long'
+                        size={16}
+                        color='white'
+                        style={styles.buttonIcon}
+                    />
+                </Pressable>
+            </View>
+        </SafeAreaView>
     );
 };
 
@@ -14,9 +37,39 @@ export default QuizScreen;
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#FDFEF4',
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         padding: 20,
     },
+    page: {
+        flex: 1,
+        backgroundColor: '#FDFEF4',
+    },
+    title: {
+        textAlign: 'center',
+        color: '#005055'
+    },
+    time: {
+        textAlign: 'center',
+        marginTop: 15,
+        color: '#005055',
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: '#005055',
+        padding: 20,
+        borderRadius: 100,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 500,
+        fontSize: 16,
+        letterSpacing: 2
+    },
+    buttonIcon: {
+        position: 'absolute',
+        right: 20
+    }
 });
